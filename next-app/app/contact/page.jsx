6457011f20e8592e48293cb7d0db3cc483a04492
Pratik Kuril <pratikkuril16@gmail.com>
2025-05-08 04:26:01 +0530

    Add contact page content

--- next-app/app/contact/page.jsx
+++ next-app/app/contact/page.jsx
@@ -1,3 +1,47 @@
 export default function Contact() {
-  return <h1>📞 Contact Page</h1>;
+  return (
+    <section>
+      <h1 className="font-bold mt-8 text-2xl">Pratik Kuril</h1>
+      <br />
+      <p className="text-md text-gray-600 w-[75%]">
+        A bit about me - I am a software engineer with a passion for web
+        development and machine learning. I love building applications that are
+        efficient, scalable, and user-friendly. In my free time, I enjoy
+        exploring new technologies.
+        <br />
+      </p>
+      <br />
+      Following are my social media handles:
+      <div className="links text-blue-500 font-semibold hover:text-blue-600 flex flex-col gap-2 mt-4 ml-2">
+        <a
+          href="https://pratikkuril.xyz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          My Portfolio 🔗
+        </a>
+        <a
+          href="https://www.linkedin.com/in/kurilpratik/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linkedin 🔗
+        </a>
+        <a
+          href="https://www.behance.net/pratikkuril"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Behance 🔗
+        </a>
+        <a
+          href="https://www.x.com/kurilpratik"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          X (Twitter) 🔗
+        </a>
+      </div>
+    </section>
+  );
 }
